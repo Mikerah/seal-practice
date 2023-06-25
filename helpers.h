6facc8642dd598213e86e90393446b84766ea525
Mikerah <mikerah@hashcloak.com>
2023-06-25 18:05:03 -0400

    Added helpers header file

---
+++ helpers.h
@@ -0,0 +1,13 @@
+#include "seal/seal.h"
+#include <iostream>
+
+using namespace std;
+using namespace seal;
+
+/*
+Helper function: Convert a value into a hexadecimal string, e.g., uint64_t(17) --> "11".
+*/
+std::string uint64_to_hex_string(std::uint64_t value)
+{
+    return seal::util::uint_to_hex_string(&value, std::size_t(1));
+}
