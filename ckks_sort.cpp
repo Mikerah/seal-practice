26dd6146034b897e7c1ba214b815f2982d822c31
Mikerah <mikerah@hashcloak.com>
2023-08-20 12:16:49 -0400

    Implemented basic inv for sorting

--- ckks_sort.cpp
+++ ckks_sort.cpp
@@ -5,60 +5,68 @@
 using namespace std;
 using namespace seal;
 
-Ciphertext inv(EncryptionParameters parms, RelinKeys relin_keys, double scale, Ciphertext x, int d) {
+Ciphertext inv(EncryptionParameters parms, RelinKeys relin_keys, double scale, Decryptor* decryptor, Ciphertext x, int d) {
 
     SEALContext context(parms);
     Evaluator evaluator(context);
     CKKSEncoder encoder(context);
 
     Plaintext one_pt, two_pt;
-    encoder.encode(1.0, scale, one_pt);
-    encoder.encode(2.0, scale, two_pt);
+    encoder.encode(1, scale, one_pt);
+    encoder.encode(2, scale, two_pt);
 
 
     Ciphertext a_0, b_0;
     evaluator.negate_inplace(x);
     evaluator.add_plain(x, two_pt, a_0);
+    evaluator.relinearize_inplace(a_0, relin_keys);
     evaluator.add_plain(x, one_pt, b_0);
-    //evaluator.rescale_to_next_inplace(a_0);
-    //evaluator.rescale_to_next_inplace(b_0);
+    evaluator.relinearize_inplace(b_0, relin_keys);
 
-    Ciphertext *tmp_a = &a_0;
-    Ciphertext *tmp_b = &b_0;
 
     for (int i = 0; i < d; i++) {
-        evaluator.square(*tmp_b, *tmp_b);
-        evaluator.relinearize_inplace(*tmp_b, relin_keys);
-        evaluator.rescale_to_next_inplace(*tmp_b);
+        
+        evaluator.square_inplace(b_0);
+        evaluator.relinearize_inplace(b_0, relin_keys);
+        evaluator.rescale_to_next_inplace(b_0);
+
+        Plaintext b_0_plain;
+        vector<double> b_0_vec;
+        (*decryptor).decrypt(b_0, b_0_plain);
+        encoder.decode(b_0_plain, b_0_vec);
+        cout << "b_0: " << b_0_vec[0] << endl;
 
         Ciphertext tmp;
 
-        evaluator.mod_switch_to_inplace(*tmp_a, (*tmp_b).parms_id());
-        evaluator.multiply(*tmp_b, *tmp_a, tmp);
+        evaluator.mod_switch_to_inplace(a_0, b_0.parms_id());
+        /* cout << "a_0 scale: " << log2(a_0.scale()) << endl;
+        cout << "b_0 scale: " << log2(b_0.scale()) << endl; */
+        b_0.scale() = scale;
+
+        evaluator.multiply(a_0, b_0, tmp);
         evaluator.relinearize_inplace(tmp, relin_keys);
         evaluator.rescale_to_next_inplace(tmp);
 
-        
-        //(*tmp_a).scale() = pow(2.0, 40);
-        //tmp.scale() = pow(2.0, 40);
+        Plaintext tmp_plain;
+        vector<double> tmp_vec;
+        (*decryptor).decrypt(tmp, tmp_plain);
+        encoder.decode(tmp_plain, tmp_vec);
+        cout << "tmp: " << tmp_vec[0] << endl;
 
-        /*
-        cout << "Scale of tmp_b: " << log2((*tmp_b).scale()) << endl;
-        cout << "Scale of tmp_a: " << log2((*tmp_a).scale()) << endl;
-        cout << "Scale of tmp: " << log2(tmp.scale()) << endl;
+        tmp.scale() = scale;
+        evaluator.mod_switch_to_inplace(a_0, tmp.parms_id());
+        evaluator.add_inplace(a_0, tmp);
+        evaluator.relinearize_inplace(a_0, relin_keys); 
+
+        Plaintext a_0_plain;
+        vector<double> a_0_vec;
+        (*decryptor).decrypt(a_0, a_0_plain);
+        encoder.decode(a_0_plain, a_0_vec);
+        cout << "a_0: " << a_0_vec[0] << endl;
     
-        cout << "Parms of tmp_b: " << (*tmp_b).parms_id() << endl;
-        cout << "Parms of tmp_a: " << (*tmp_a).parms_id() << endl;
-        cout << "Parms of tmp: " << tmp.parms_id() << endl;
-        */
-
-        tmp.scale() = pow(2.0, 40);
-        evaluator.mod_switch_to_inplace(*tmp_a, tmp.parms_id());
-        evaluator.add_inplace(*tmp_a, tmp);
-        evaluator.relinearize_inplace(*tmp_a, relin_keys);
     }
 
-    return *tmp_a;
+    return a_0;
 }
 
 void main_inv()
@@ -69,7 +77,7 @@ void main_inv()
     size_t poly_modulus_degree = 16384;
     parms.set_poly_modulus_degree(poly_modulus_degree);
     // cout << CoeffModulus::MaxBitCount(poly_modulus_degree) << endl;
-    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 60, 60, 60, 60, 50}));
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 40, 40, 40, 40, 60}));
     
     // parms.set_coeff_modulus(CoeffModulus::CKKSDefault(poly_modulus_degree));
 
@@ -100,7 +108,7 @@ void main_inv()
     // Encode two floats as plaintexts.
     Plaintext plain1, plain2;
     encoder.encode(0.5, scale, plain1);
-    encoder.encode(4.0, scale, plain2);
+    encoder.encode(0.25, scale, plain2);
 
     // Encrypt the plaintexts.
     Ciphertext cipher1, cipher2;
@@ -108,13 +116,13 @@ void main_inv()
     encryptor.encrypt(plain2, cipher2);
 
     // Perform inv on ciphertexts.
-    Ciphertext inv_05 = inv(parms, relin_keys, scale, cipher1, 5);
-    Ciphertext inv_4 = inv(parms, relin_keys, scale, cipher2, 5);
+    Ciphertext inv_05 = inv(parms, relin_keys, scale, &decryptor, cipher1, 5);
+    Ciphertext inv_025 = inv(parms, relin_keys, scale, &decryptor, cipher2, 5);
 
     // Decrypt the result.
     Plaintext plain_result1, plain_result2;
     decryptor.decrypt(inv_05, plain_result1);
-    decryptor.decrypt(inv_4, plain_result2);
+    decryptor.decrypt(inv_025, plain_result2);
 
     // Decode the result.
     vector<double> result1, result2;
@@ -122,9 +130,10 @@ void main_inv()
     encoder.decode(plain_result2, result2);
 
     cout << "Actual result1: " << result1[0] << endl;
+    //print_vector(result1);
     cout << "Expected result1: 2" << endl;
 
-    cout << "Actual result2: " << result2[0] << endl;
-    cout << "Expected result2: 0.25" << endl;
+    cout << "Actual result1: " << result2[0] << endl;
+    cout << "Expected result2: 4" << endl;
 
 }
